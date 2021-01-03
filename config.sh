6b181e755b5de3f6983d1afe9bf09d4bfea56608
Benjamin Miramontes <benjamin@miramontes.dev>
2021-01-03 10:16:40 -0500

    Added config.sh

---
+++ config.sh
@@ -0,0 +1,13 @@
+# Domain and port for WSS
+DOMAIN=''
+WSS_PORT=14501
+
+# HTML root for your webserver. The nginx default
+# on ubuntu is /var/www/html.
+HTML_ROOT='/var/www/html'
+
+# Full cert chain for your domain
+FULLCHAIN="/etc/letsencrypt/live/$DOMAIN/fullchain.pem"
+
+# Private key for your domain cert
+PRIVKEY="/etc/letsencrypt/live/$DOMAIN/privkey.pem"
